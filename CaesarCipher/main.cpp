8eb5f4361abc63b09b851802b2bd862e31122c4b
Vi Tiet <vitiet.programmer@gmail.com>
2020-08-17 17:52:02 -0400

    Add Loop Functionality

--- CaesarCipher/main.cpp
+++ CaesarCipher/main.cpp
@@ -1,44 +1,67 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdio>
 #include "FCaesarCipher.h"
 
 int main()
 {
+    bool isRunning = true;
     std::string Answer = "";
-    std::cout << "Enter Text: ";
-    std::getline(std::cin, Answer);
+    char ynAnswer = ' ';
 
-    std::cout << "\nProcedurally Shifting Code\n";
-    std::cout << "[DECRYPTION] Caesar Code -> Alphabet:\n";
-
-    int Shift = SIZE;
-
-    for (size_t i = 0; i < Shift; i++)
+    while (isRunning)
     {
-        std::cout << "[SHIFT RIGHT " << i << "]: " << CaesarCipher::Decrypt(i, true, Answer) << std::endl;
-    }
+        std::cout << "Enter Text: ";
+        std::getline(std::cin, Answer);
 
-    std::cout << std::endl;
+        std::cout << "\nProcedurally Shifting Code\n";
+        std::cout << "[DECRYPTION] Caesar Code -> Alphabet:\n";
 
-    for (size_t i = 0; i < Shift; i++)
-    {
-        std::cout << "[SHIFT LEFT  " << i << "]: " << CaesarCipher::Decrypt(i, false, Answer) << std::endl;
+        int Shift = SIZE;
 
-    }
+        for (size_t i = 0; i < Shift; i++)
+        {
+            std::cout << "[SHIFT RIGHT " << i << "]: " << CaesarCipher::Decrypt(i, true, Answer) << std::endl;
+        }
 
-    std::cout << "\nProcedurally Shifting Code\n";
-    std::cout << "[ENCRYPTION] Alphabet -> Caesar Code:\n";
+        std::cout << std::endl;
 
-    for (size_t i = 0; i < Shift; i++)
-    {
-        std::cout << "[SHIFT RIGHT " << i << "]: " << CaesarCipher::Encrypt(i, true, Answer) << std::endl;
-    }
+        for (size_t i = 0; i < Shift; i++)
+        {
+            std::cout << "[SHIFT LEFT  " << i << "]: " << CaesarCipher::Decrypt(i, false, Answer) << std::endl;
 
-    std::cout << std::endl;
+        }
 
-    for (size_t i = 0; i < Shift; i++)
-    {
-        std::cout << "[SHIFT LEFT  " << i << "]: " << CaesarCipher::Encrypt(i, false, Answer) << std::endl;
+        std::cout << "\nProcedurally Shifting Code\n";
+        std::cout << "[ENCRYPTION] Alphabet -> Caesar Code:\n";
+
+        for (size_t i = 0; i < Shift; i++)
+        {
+            std::cout << "[SHIFT RIGHT " << i << "]: " << CaesarCipher::Encrypt(i, true, Answer) << std::endl;
+        }
+
+        std::cout << std::endl;
+
+        for (size_t i = 0; i < Shift; i++)
+        {
+            std::cout << "[SHIFT LEFT  " << i << "]: " << CaesarCipher::Encrypt(i, false, Answer) << std::endl;
+        }
+
+        do
+        {
+            std::cout << "\nConvert Other Text? [y/n] ";
+            std::getline(std::cin, Answer);
+            ynAnswer = tolower(Answer[0]);
+        } while (ynAnswer != 'y' && ynAnswer != 'n');
+
+        if (ynAnswer == 'n')
+        {
+            isRunning = false;
+        }
+        else
+        {
+            system("cls");
+        }
     }
 }
