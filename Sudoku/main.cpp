4826b557b196da1e66c1536af67eac89c43b5430
Vi Tiet <vitiet.programmer@gmail.com>
2020-08-17 18:14:48 -0400

    Add Loop Functionality

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -1,28 +1,52 @@
+#include "cstdio"
 #include "main.h"
 #include "FSudokuGenerator.h"
 
 int main()
 {
+	std::string Answer = "";
+	char ynAnswer = ' ';
+	bool isRunning = true;
+
 	FSudoku Sudoku = FSudoku();
 
-	Sudoku.Generate(EMode::EASY);
-	std::cout << "Mode: SNOW FLAKE" << std::endl;
-	Sudoku.PrintSudoku();
-	std::cout << "\nSolution:\n";
-	Sudoku.PrintSolution();
-	std::cout << std::endl;
+	while (isRunning)
+	{
+		Sudoku.Generate(EMode::EASY);
+		std::cout << "Mode: SNOW FLAKE" << std::endl;
+		Sudoku.PrintSudoku();
+		std::cout << "\nSolution:\n";
+		Sudoku.PrintSolution();
+		std::cout << std::endl;
 
-	Sudoku.Generate(EMode::NORMAL);
-	std::cout << "Mode: SO-SO" << std::endl;
-	Sudoku.PrintSudoku();
-	std::cout << "\nSolution:\n";
-	Sudoku.PrintSolution();
-	std::cout << std::endl;
+		Sudoku.Generate(EMode::NORMAL);
+		std::cout << "Mode: SO-SO" << std::endl;
+		Sudoku.PrintSudoku();
+		std::cout << "\nSolution:\n";
+		Sudoku.PrintSolution();
+		std::cout << std::endl;
 
-	Sudoku.Generate(EMode::HARD);
-	std::cout << "Mode: IMPOSSIBRUUU" << std::endl;
-	Sudoku.PrintSudoku();
-	std::cout << "\nSolution:\n";
-	Sudoku.PrintSolution();
-	std::cout << std::endl;
+		Sudoku.Generate(EMode::HARD);
+		std::cout << "Mode: IMPOSSIBRUUU" << std::endl;
+		Sudoku.PrintSudoku();
+		std::cout << "\nSolution:\n";
+		Sudoku.PrintSolution();
+		std::cout << std::endl;
+
+		do
+		{
+			std::cout << "Generate More? [y/n] ";
+			std::cin >> Answer;
+			ynAnswer = tolower(Answer[0]);
+		} while (ynAnswer != 'y' && ynAnswer != 'n');
+
+		if (ynAnswer == 'n')
+		{
+			isRunning = false;
+		}
+		else
+		{
+			system("cls");
+		}
+	}
 }
